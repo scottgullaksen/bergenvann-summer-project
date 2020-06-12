1e13a68193c2cee2ba78fd88b379bfe83413df53
scottgullaksen <scott.gullaksen@gmail.com>
2020-06-12 10:53:07 +0200

    Added logger

--- data/preprocessor.py
+++ data/preprocessor.py
@@ -1,5 +1,16 @@
 import os
 import csv
+import logging
+import logging.config
+import yaml
+
+with open('./logging.yaml', 'r') as f:
+	log_cfg = yaml.safe_load(f.read())
+
+logging.config.dictConfig(log_cfg)
+
+logger = logging.getLogger(__name__)
+
 
 class CSVFileReader(object):
 
