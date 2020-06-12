64f4dcd4db65a5532452be892d556e805a776deb
scottgullaksen <scott.gullaksen@gmail.com>
2020-06-12 11:58:01 +0200

    #7 - constructor works

--- data/preprocessor.py
+++ data/preprocessor.py
@@ -9,33 +9,46 @@ with open('./logging.yaml', 'r') as f:
 
 logging.config.dictConfig(log_cfg)
 
-logger = logging.getLogger(__name__)
-
 
 class CSVFileReader(object):
 
 	def __init__(self, path= None):
 
+		logger = logging.getLogger(__name__)
+
 		# Set path to raw corpus
-		path = os.path.join(__file__, path) if not path else path
+		path = os.path.join(
+			os.path.dirname(os.path.abspath(__file__)), 'raw_data'
+		) if not path else path
+
+		print(path)
+
+		logger.info(f'Instantiating with path: {path}')
 
 		# Get all filepaths in dir if path is a dir
-		if os.path.isdir(path):
-			self.paths = [
-				os.path.join(filepath)
-				for filepath in os.listdir(path) if os.path.isfile(
-					os.path.join(path, filepath)
-				)
-			]
-		else: self.paths = [path]
+		self.paths = [
+			os.path.join(os.path.abspath(dirpath), filename)
+			for dirpath, dirnames, filenames in os.walk(path)
+			for filename in filenames if os.path.splitext(filename)[1] == '.csv'
+		] if os.path.isdir(path) else [path]
 
-		# Extract only csv files
-		self.paths = filter(lambda f: os.path.splitext(f)[1] == 'csv', self.path)
+		print(self.paths)
 
-		if len(self.path) == 0:
+		logger.info(f'csv files found: {self.paths}')
+
+		if len(self.paths) == 0:
 			raise ValueError("The provided directory/file contained no csv files")
 	
 	def yield_rows(self, p):
 		for path in self.paths:
 			with open(path) as csvfile:
 				dataset = csv.reader(csvfile, delimitter= ',')
+
+
+if __name__ == '__main__':
+
+	path = os.path.join(
+		os.path.dirname(os.path.abspath(__file__)), 'raw_data', 'pumpedata' , 'GeorgernesVerft.csv'
+	)
+
+	reader = CSVFileReader(path)
