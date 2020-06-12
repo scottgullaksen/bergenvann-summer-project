4877b38a9752700b77baed7fde053347618aba6f
scottgullaksen <scott.gullaksen@gmail.com>
2020-06-12 09:44:43 +0200

    #7 - created class

---
+++ data/preprocessor.py
@@ -0,0 +1,30 @@
+import os
+import csv
+
+class CSVFileReader(object):
+
+	def __init__(self, path= None):
+
+		# Set path to raw corpus
+		path = os.path.join(__file__, path) if not path else path
+
+		# Get all filepaths in dir if path is a dir
+		if os.path.isdir(path):
+			self.paths = [
+				os.path.join(filepath)
+				for filepath in os.listdir(path) if os.path.isfile(
+					os.path.join(path, filepath)
+				)
+			]
+		else: self.paths = [path]
+
+		# Extract only csv files
+		self.paths = filter(lambda f: os.path.splitext(f)[1] == 'csv', self.path)
+
+		if len(self.path) == 0:
+			raise ValueError("The provided directory/file contained no csv files")
+	
+	def yield_rows(self, p):
+		for path in self.paths:
+			with open(path) as csvfile:
+				dataset = csv.reader(csvfile, delimitter= ',')
