ee137d6326812a5de53ec751c050c42c7ab11b86
scottgullaksen <scott.gullaksen@gmail.com>
2020-08-06 09:54:53 +0200

    remove setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-# So relative import are made possible
-
-# In same directory as this file, run:
-# $ pip install -e .
-
-from setuptools import setup, find_packages
-
-setup(name='myproject', version='1.0', packages=find_packages())
